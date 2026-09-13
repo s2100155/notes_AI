ff705a8f5e1cec60b455f689f942b066397dcd5f
Kenlie <s2100155@usls.edu.ph>
2026-09-13 16:20:37 +0800

    initial commit

---
+++ begginers.py
@@ -0,0 +1,53 @@
+#course = "pass the exam"
+#print(course[1:-1])
+#print(10>3)
+
+
+
+#water = 130
+#if water < 20:
+ #   print("fuck this water, it's so good")
+#    print("i like it!")
+#elif water > 20:
+#    print("nigga this water sucks")
+#    print("like legit")
+#else:
+ #   print("yessir")
+
+
+
+#for number in range(20):
+    #print("nigga" , number)
+
+#print(type(range(5)))
+
+#command  = ""
+
+#while command != "quit":#
+#    command = input(">")
+ #   print("ECHO",)
+ 
+
+#count = 0
+#for number in range(1, 10):
+   # if number % 2 == 0:
+   #     count += 1
+   #     print(number)
+#print(f"we have counted {count} even number")
+
+
+#def greet(first_name, last_name):
+   # print(f"hi {first_name} {last_name}")
+   # print("welcome to mobile legends")
+
+#greet("kenlie", "maghari")
+
+
+#raw = "7"
+#n = int(raw) + 3
+#print(f"result: {n}")
+
+grades = {"amara": 92}
+print(grades.get("leo", "N/A0"))
+print({1, 2, 3} & {2, 3, 4})
+
---
+++ practice.py
@@ -0,0 +1,60 @@
+#Book - title (str), isbn (int), is_checked_out ()DONE
+#Member - borrow_book, return_book(book) -> functions DONE
+#BookUnavailableError -> Custom Exception Handler
+ #condition: cannot borrow unavailbale book, message + book title DONE
+
+#Task:
+#1. add __str__ on Book, shows title and availability DONE
+
+#Results:
+#1. successful borrow (DONE)
+#2. successful return (DONE)
+#3. failed borrow attempt on checked out book (DONE)
+
+class Book:
+    def __init__(self, title, isbn):
+        self.title = title
+        self.isbn = isbn
+        self.is_checked_out = False
+
+    def __str__(self):
+        status = "checked out" if self.is_checked_out else "available"
+        return f"{self.title} ({status})"
+
+class Member:
+    def __init__(self, name):
+        self.name = name
+        self.borrowed_books = []
+
+    def borrow_book(self, book: Book):
+        if book.is_checked_out:
+            raise BookUnavailableError(book.title)
+        book.is_checked_out = True
+        self.borrowed_books.append(book)
+
+    def return_book(self, book: Book):
+        if book in self.borrowed_books:
+            book.is_checked_out = False
+            self.borrowed_books.remove(book)
+
+class BookUnavailableError(Exception):
+    def __init__(self, title):
+        super().__init__(f"{title} is checked out and unavailable.")
+        self.title = title
+
+if __name__ == "__main__":
+    hobbit = Book("Hobbit", "12345")
+    kendra = Member("Kendra")
+    gwen = Member("Gwen")
+
+    kendra.borrow_book(hobbit)
+    print(hobbit)
+
+    kendra.return_book(hobbit)
+    print(hobbit)
+
+    kendra.borrow_book(hobbit)
+    try:
+        gwen.borrow_book(hobbit)
+    except BookUnavailableError as e:
+        print(f"Borrow failed: {e}")
